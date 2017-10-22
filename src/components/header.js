dca83284ff0d05262690f0afee0fd8d4f55084af
Mislav Kucanda <multicolor.anthill@gmail.com>
2017-10-22 20:50:00 +0200

    Profile desplay on home page redisign.

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,13 @@ export default class Header extends Component {
 	constructor(props) {
 		super(props);
 
+    this.state = {
+      hooveredElement: '',
+    }
+
 		this.renderNavButtons = this.renderNavButtons.bind(this);
+    this.onHooverElement = this.onHooverElement.bind(this);
+    this.onStopHooverElement = this.onStopHooverElement.bind(this);
 		this.onLogOut = this.onLogOut.bind(this);
 	}
 
@@ -23,8 +29,17 @@ export default class Header extends Component {
   		});
 	}
 
+  onHooverElement(elem, label) {
+    this.setState({ hooveredElement: label });
+  }
+
+  onStopHooverElement() {
+    this.setState({ hooveredElement: '' });
+  }
+
 	renderNavButtons() {
     const { isAuthenticated } = this.props;
+    const { hooveredElement } = this.state;
     const {
       homeNavUrl,
       registerNavUrl,
@@ -39,46 +54,67 @@ export default class Header extends Component {
     if(isAuthenticated) {
       navButtons = (
         <div className='float-right'>
-          <img src={homeNavUrl} onClick={() => this.props.navigateToPage('home')} style={{ width: 75, height: 75 }} />
-          <img src={profilNavUrl} onClick={() => this.props.navigateToPage('profile')} />
-          <img src={logoutNavUrl} onClick={() => this.onLogOut()} />
+          <img 
+            src={homeNavUrl} 
+            onClick={() => this.props.navigateToPage('home')} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'home' ? '#5c9b8e' : null }} 
+            onMouseOver={(elem) => this.onHooverElement(elem, 'home')} 
+            onMouseOut={this.onStopHooverElement}
+          />
+          <img 
+            src={profilNavUrl} 
+            onClick={() => this.props.navigateToPage('profile')} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'profile' ? '#5c9b8e' : null }}
+            onMouseOver={(elem) => this.onHooverElement(elem, 'profile')}
+            onMouseOut={this.onStopHooverElement}
+          />
+          <img 
+            src={logoutNavUrl} 
+            onClick={() => this.onLogOut()} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'logout' ? '#5c9b8e' : null }}
+            onMouseOver={(elem) => this.onHooverElement(elem, 'logout')}
+            onMouseOut={this.onStopHooverElement}
+          />
         </div>
       );
     } else {
       navButtons = (
         <div className='float-right'>
-          <img src={homeNavUrl} onClick={() => this.props.navigateToPage('home')} style={{ width: 60, height: 60 }} />
-          <img src={registerNavUrl} onClick={() => this.props.navigateToPage('register')} style={{ width: 60, height: 60 }} />
-          <img src={loginNavUrl} onClick={() => this.props.navigateToPage('login')} style={{ width: 60, height: 60 }} />
+          <img 
+            src={homeNavUrl} 
+            onClick={() => this.props.navigateToPage('home')} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'home' ? '#5c9b8e' : null }} 
+            onMouseOver={(elem) => this.onHooverElement(elem, 'home')}
+            onMouseOut={this.onStopHooverElement}
+          />
+          <img 
+            src={registerNavUrl} 
+            onClick={() => this.props.navigateToPage('register')} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'register' ? '#5c9b8e' : null }} 
+            onMouseOver={(elem) => this.onHooverElement(elem, 'register')}
+            onMouseOut={this.onStopHooverElement}
+          />
+          <img 
+            src={loginNavUrl} 
+            onClick={() => this.props.navigateToPage('login')} 
+            style={{ width: 60, height: 60, backgroundColor: hooveredElement === 'login' ? '#5c9b8e' : null }} 
+            onMouseOver={(elem) => this.onHooverElement(elem, 'login')}
+            onMouseOut={this.onStopHooverElement}
+          />
         </div>
       );
     }
     return navButtons;
   }
 
-
 	render() {
 		return(
 			<div className="header clearfix" style={{ backgroundColor: '#36B39C'}}>
-        <div className="container">
+        <div>
         {this.renderNavButtons()}
-        
+        <img src={Const.logoUrl} style={{ hight: 60, width: 300 }} />
         </div>
       </div>
     );
 	}
 }
-//<img src={Const.logoUrl} className="py-4" style={{ hight: 32, width: 200 }} />
-// <nav>
-//           <ul className="nav nav-pills float-right">
-//             <li className="nav-item">
-//               <button type="button" className="btn btn-light" onClick={() => this.props.navigateToPage('home')}>Home</button>
-//             </li>
-//             <li className="nav-item">
-//               <button type="button" className="btn btn-light" onClick={() => this.props.navigateToPage('register')}>Register</button>
-//             </li>
-//             <li className="nav-item">
-//               <button type="button" className="btn btn-light" onClick={() => this.props.navigateToPage('login')}>Login</button>
-//             </li>
-//           </ul>
-//         </nav>
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -11,11 +11,13 @@ export default class HomePage extends Component {
 			selectedCategory: '',
 		};
 
-		this.renderAreas = this.renderAreas.bind(this);
+		this.renderCategories = this.renderCategories.bind(this);
 		this.getProfiles = this.getProfiles.bind(this);
 		this.onMouseOverCategory = this.onMouseOverCategory.bind(this);
 		this.onMouseOutCategory = this.onMouseOutCategory.bind(this);
 		this.onSelectCategory = this.onSelectCategory.bind(this);
+		this.renderProfileInfo = this.renderProfileInfo.bind(this);
+		this.renderProfileImage = this.renderProfileImage.bind(this);
 	}
 
 	componentDidMount() {
@@ -37,7 +39,7 @@ export default class HomePage extends Component {
 			this.setState({ selectedCategory: label });
 	}
 
-	renderAreas() {
+	renderCategories() {
 		return(
 			<div className="row">
 	      {Const.categories.map((category, index) => {
@@ -83,21 +85,44 @@ export default class HomePage extends Component {
 		});
 	}
 
+	renderProfileInfo(iconUrl, info) {
+		if (info !== null && info !== '') {
+	     return (
+		    <li className="list-group-item p-1">
+		    	<img src={iconUrl} style={{ width:50, height: 50 }} alt=""/>
+		    	<span className="pl-1">{info}</span>
+		    </li>
+		   );
+	  }
+	  return null;
+	}
+
+	renderProfileImage(imgUrl) {
+		if(imgUrl !== null && imgUrl !== '') {
+			return <img className="card-img-top img-fluid" src={imgUrl} alt="" />
+		}
+		return null;
+	}
+
 	renderProfiles() {
 		const { profiles } = this.state;
+
 		return (
 			<div className="card-columns">
 				{profiles.map((profile, index) => {
 					if(this.state.selectedCategory === '' || profile.category.includes(this.state.selectedCategory))
 						return(
-							<div className="card" key={index}>
-	    					<img className="card-img-top img-fluid" src={profile.imgUrl} alt="" />
+							<div className="card mb-3" key={index}>
+	    					{this.renderProfileImage(profile.imgUrl)}
 	    					<div className="card-block p-2">
-	      					<h4 className="card-title">{profile.username}</h4>
+	      					<h4 className="card-title text-center">{profile.username}</h4>
 	      					<p className="card-text">{profile.description}</p>
-	      					<p className="card-text">Lokacija: {profile.address}</p>
-	      					<p className="card-text">Kontakt: {profile.email} {profile.mobilePhone}</p>
 	    					</div>
+	    					<ul className="list-group list-group-flush">
+	    						{this.renderProfileInfo(Const.profileLogos.location, profile.address)}
+	    						{this.renderProfileInfo(Const.profileLogos.phone, profile.mobilePhone)}
+	    						{this.renderProfileInfo(Const.profileLogos.email, profile.email)}
+	    					</ul>
 	  					</div>
 						);
 				})}
@@ -110,7 +135,7 @@ export default class HomePage extends Component {
 			<div className="container px-5">
 				<p className="text-center mb-0 mt-4">ODABERI PODRUČJE</p>
 				<hr className="mt-0" />
-	      {this.renderAreas()}
+	      {this.renderCategories()}
 	      <p className="text-center mb-0 mt-3">ODABERI INSTRUKTORA</p>
 				<hr className="mt-0" />
 				{this.renderProfiles()}
